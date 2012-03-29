59eac4339e9ff56d3acc108cf18c7604f50b5b58
David Doria <daviddoria@gmail.com>
2012-03-29 15:09:08 -0400

    update.

--- VTKHelpers.h
+++ VTKHelpers.h
@@ -28,6 +28,13 @@ namespace VTKHelpers
 extern unsigned char TRANSPARENT;
 extern unsigned char OPAQUE;
 
+
+/** Write a vtkPolyData to a .vtp file. */
+void WritePolyData(vtkPolyData* const polyData, const std::string& fileName);
+
+/** Write a vtkImageData to a .vti file. */
+void WriteImageData(vtkImageData* const imageData, const std::string& fileName);
+
 void InitializeStaticVariables();
 
 void GetCellCenter(vtkImageData* const imageData, const unsigned int cellId, double center[3]);
